f49ea425f00b17f78e99e679641b324a589d81df
Nilesh Gule <vn_nilesh@yahoo.com>
2016-04-16 20:23:34 +0800

    Added console client to Martingate

---
+++ MartingaleConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using MartingaleTheory;
+using System;
+using System.Collections.Generic;
+
+namespace MartingaleConsoleClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            double initialAmount = 1;
+            double payoutPercentage = 0.70;
+            int numberOfRecoveries = 10;
+            MartingaleStrategy martingale = new MartingaleStrategy(initialAmount, payoutPercentage, numberOfRecoveries);
+
+            List<Recovery> recoveries = martingale.Recoveries;
+
+            foreach (Recovery recovery in recoveries)
+            {
+                Console.WriteLine("Recovery number : {0}, TradeAmount : {1}, CommulativeLoss : {2}", recovery.RecoveryNumber, recovery.TradeAmount, recovery.CummulativeLoss);
+            }
+        }
+    }
+}
